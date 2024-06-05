f5bf8b282082f126e1d967e660332fdf8cce9304
yuwei-3206 <122844465+yuwei-3206@users.noreply.github.com>
2024-06-05 17:11:32 -0400

    Delete travel-mng directory

--- travel-mng/mng-server/travel_plans/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class TravelPlan(db.Model):
-    id = db.Column(db.String(36), primary_key=True)
-    location = db.Column(db.String(100), nullable=False)
-    days = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
